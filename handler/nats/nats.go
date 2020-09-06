ec1a116d9fb519a3fc420b81682f2327d6c81e75
Parsa Eskandarnejad <parsaaes@gmail.com>
2020-09-07 03:31:12 +0430

    Add subscriber package

---
+++ handler/nats/nats.go
@@ -0,0 +1,25 @@
+package nats
+
+import "github.com/nats-io/nats.go"
+
+type Handler struct {
+	subject string
+	outputs []chan<- []byte
+}
+
+func New(subject string, outputs []chan<- []byte) *Handler {
+	return &Handler{
+		subject: subject,
+		outputs: outputs,
+	}
+}
+
+func (s *Handler) Handle(msg *nats.Msg) {
+	for _, ch := range s.outputs {
+		ch <- msg.Data
+	}
+}
+
+func (s *Handler) Subject() string {
+	return s.subject
+}
